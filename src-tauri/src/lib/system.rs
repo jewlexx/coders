f47fcfcf3ef4eac24140b63331e4a9f35fdf2b3d
jamesinaxx <grassojames5@gmail.com>
2022-02-21 14:51:15 +1100

    fix: bug fix

--- src-tauri/src/lib/system.rs
+++ src-tauri/src/lib/system.rs
@@ -1,7 +1,8 @@
-use std::path::Path;
+use std::path::PathBuf;
 
-pub fn get_config_dir() -> &'static Path {
+pub fn get_config_dir() -> PathBuf {
     directories::ProjectDirs::from("com", "jewlexx", "Coders")
         .unwrap()
         .project_path()
+        .to_path_buf()
 }
