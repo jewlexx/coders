ff57e01643f4f58f32f2c268c3841bb6913cfd35
jamesinaxx <grassojames5@gmail.com>
2022-02-21 14:45:49 +1100

    chore: moved to a different system

--- src-tauri/src/lib/system.rs
+++ src-tauri/src/lib/system.rs
@@ -1,12 +1,7 @@
-use std::path::PathBuf;
+use std::path::Path;
 
-pub fn get_config_dir() -> PathBuf {
-    let mut path = directories::BaseDirs::new()
+pub fn get_config_dir() -> &'static Path {
+    directories::ProjectDirs::from("com", "jewlexx", "Coders")
         .unwrap()
-        .config_dir()
-        .to_path_buf();
-
-    path.push("Coders");
-
-    path.clone()
+        .project_path()
 }
